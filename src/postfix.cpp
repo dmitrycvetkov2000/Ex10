77817a85602cc9164a2b27cd74b0439d84d3e132
dmitrycvetkov2000 <dmitrycvetkov2000@mail.ru>
2021-01-20 19:25:55 +0300

    cpplint

--- src/postfix.cpp
+++ src/postfix.cpp
@@ -7,57 +7,70 @@
 #include <string>
 #include <vector>
 
-bool isOp(const std::string& str)
-{
-    return str.length() == 1 && (str == "(" || str == ")" || str == "+" || str == "-" || str == "*" || str == "/");
+bool isOp(const std::string& str) {
+    return str.length() == 1 &&
+        (str == "(" ||
+            str == ")" ||
+            str == "+" ||
+            str == "-" ||
+            str == "*" ||
+            str == "/");
 }
 
-int priority(const char& op)
-{
+int priority(const char& op) {
     if (op == '(')
         return 0;
     else if (op == ')')
         return 1;
     else if (op == '+' || op == '-')
         return 2;
-    else // (op == '*' || op == '/')
+    else  // (op == '*' || op == '/')
         return 3;
 }
 
-std::vector<std::string> tokenize(const std::string& str)
-{
+std::vector<std::string> tokenize(const std::string& str) {
     std::vector<std::string> tokens;
     std::optional<int> num;
     int decimal_fraction = 0;
     bool is_dev_frac = false;
-    const auto push_number = [&num, &tokens, &decimal_fraction, &is_dev_frac]()
-    {
+    const auto push_number = [
+        &num,
+        &tokens,
+        &decimal_fraction,
+        &is_dev_frac
+    ]() {
         if (num.has_value())
-            tokens.push_back(std::to_string(*num) + (is_dev_frac ? (std::string(".") + std::to_string(decimal_fraction)) : std::string()));
+            tokens.push_back(std::to_string(*num) +
+                (is_dev_frac ?
+                    (std::string(".") + std::to_string(decimal_fraction)) :
+                    std::string()));
         num.reset();
         decimal_fraction = 0;
         is_dev_frac = false;
     };
-    std::for_each(std::begin(str), std::end(str), [&num, &tokens, &push_number, &is_dev_frac, &decimal_fraction](const char& ch)
-        {
-            if (isOp({ ch }))
-            {
+    std::for_each(std::begin(str), std::end(str), [
+        &num,
+        &tokens,
+        &push_number,
+        &is_dev_frac,
+        &decimal_fraction
+    ](const char& ch) {
+            if (isOp({ ch })) {
                 push_number();
                 tokens.push_back({ ch });
-            }
-            else if (ch >= '0' && ch <= '9')
-            {
+            } else if (ch >= '0' && ch <= '9') {
                 if (!num.has_value())
                     num.emplace(0);
                 if (!is_dev_frac)
-                    *num = *num * 10 + int(ch - '0');
+                    *num = *num * 10 + static_cast<int>(ch - '0');
                 else
-                    decimal_fraction = decimal_fraction * 10 + int(ch - '0');
-            }
-            else if (ch == ' ')
+                    decimal_fraction = decimal_fraction * 10 +
+                        static_cast<int>(ch - '0');
+            } else if (ch == ' ') {
                 push_number();
-            else if (ch == '.')
+            } else if (ch == '.') {
                 is_dev_frac = true;
+            }
         });
     push_number();
     return tokens;
@@ -67,30 +80,27 @@ std::string infix2postfix(std::string infix) {
     std::string postfix;
     const auto tokens = tokenize(infix);
     MyStack<char> opStack(1);
-    std::for_each(std::begin(tokens), std::end(tokens), [&opStack, &postfix](const std::string& token)
-        {
-            if (isOp(token))
-            {
-                if (token == ")")
-                {
-                    while (opStack.get() != '(')
-                    {
+    std::for_each(std::begin(tokens), std::end(tokens),
+        [&opStack, &postfix](const std::string& token) {
+            if (isOp(token)) {
+                if (token == ")") {
+                    while (opStack.get() != '(') {
                         postfix += opStack.get();
                         postfix += ' ';
                         opStack.pop();
                     }
                     opStack.pop();
-                }
-                else
-                {
+                } else {
                     const int priorityCurrent = priority(token[0]);
-                    if (priorityCurrent == 0 || opStack.isEmpty() || priorityCurrent > priority(opStack.get()))
+                    if (priorityCurrent == 0 ||
+                        opStack.isEmpty() ||
+                        priorityCurrent > priority(opStack.get())) {
                         opStack.push(token[0]);
-                    else
-                    {
+                    } else {
                         int priorityPrev;
-                        while (!opStack.isEmpty() && (priorityPrev = priority(opStack.get())) >= priorityCurrent)
-                        {
+                        while (!opStack.isEmpty() &&
+                            (priorityPrev = priority(opStack.get()))
+                            >= priorityCurrent) {
                             postfix += opStack.get();
                             postfix += ' ';
                             opStack.pop();
@@ -99,15 +109,12 @@ std::string infix2postfix(std::string infix) {
                     }
                 }
 
-            }
-            else
-            {
+            } else {
                 postfix += token;
                 postfix += ' ';
             }
         });
-    while (!opStack.isEmpty())
-    {
+    while (!opStack.isEmpty()) {
         postfix += opStack.get();
         postfix += ' ';
         opStack.pop();

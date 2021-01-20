56f9b4c83878f6ffef9ed36a49de326561c13e66
dmitrycvetkov2000 <dmitrycvetkov2000@mail.ru>
2021-01-20 19:16:00 +0300

    postfix notation

--- include/MyStack.h
+++ include/MyStack.h
@@ -2,8 +2,72 @@
 #ifndef INCLUDE_MYSTACK_H_
 #define INCLUDE_MYSTACK_H_
 
-template<class T>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+
+template <typename T>
 class MyStack {
+public:
+    explicit MyStack(const size_t stackSize) {
+        if (!stackSize)
+            throw std::logic_error("Null size is not allowed");
+        capacity = stackSize;
+        size = 0;
+        tail = new T[capacity];
+    }
+
+    MyStack(const MyStack& other) : tail(new T[other.capacity]),
+        size(other.size),
+        capacity(other.capacity) {
+        for (size_t i = 0; i < size; ++i)
+            tail[i] = other.tail[i];
+    }
+
+    ~MyStack() {
+        delete[] tail;
+    }
+
+    T get() const {
+        if (isEmpty())
+            throw std::out_of_range("Stack is empty");
+        return tail[size - 1];
+    }
+
+    T pop() {
+        const T value = get();
+        --size;
+        return value;
+    }
+
+    void push(const T& elem) {
+        if (size == capacity) {
+            T* newTail = new T[capacity *= 2];
+            if (!newTail) {
+                std::free(tail);
+                throw std::bad_alloc();
+            }
+            std::memcpy(newTail, tail, size * sizeof(T));
+            tail = newTail;
+        }
+        tail[size] = elem;
+        ++size;
+    }
+
+    bool isFull() const {
+        // memory reallocation is implemented
+        // stack is never full as long as there is enough memory to store it
+        return false;
+    }
+
+    bool isEmpty() const {
+        return !size;
+    }
+
+private:
+    T* tail;
+    size_t size;
+    size_t capacity;
 };
 
 #endif  // INCLUDE_MYSTACK_H_
--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,10 @@
 #include "postfix.h"
 
 int main() {
+    std::string inf("(2 + 60) * 3 / (4.23 - 2 )");
+    std::cout << "Infix: " << inf << std::endl;
+    std::string postf = infix2postfix(inf);
+    std::cout << "Postfix:  " << postf << std::endl;
+    std::cout << "Expected: 2 60 + 3 * 4.23 2 - /" << std::endl;
   return 0;
 }
